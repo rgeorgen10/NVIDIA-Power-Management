330540a295de99def3b44d8553b814a4cfc62fe2
Riley Georgen <rgeorgen10@gmail.com>
2026-01-18 21:21:15 -0700

    Add Python File

---
+++ NVIDIA Power Management.py
@@ -0,0 +1,12 @@
+"""Python Application To Make The NVIDIA  Driver Ues Fine Power Management"""
+
+import sys
+
+def main:
+    print("   |-------------------------------------------------------|")
+    print("   |     NVIDIA - ENABLE FINE-GRAINED POWER MANAGEMENT     |")
+    print("   |-------------------------------------------------------|")
+    print("   | REQUIREMENTS:                                         |")
+    print("   | 1) Have the NVIDIA Proprietary Driver Installed"      |")
+
+    
