58d18cadd11f3230bd858187fb8f4f4a8b696e85
Riley Georgen <rgeorgen10@gmail.com>
2026-01-19 21:26:47 -0700

    Added checkDriver Function

--- NVIDIA Power Management.py
+++ NVIDIA Power Management.py
@@ -1,12 +1,35 @@
 """Python Application To Make The NVIDIA  Driver Ues Fine Power Management"""
 
 import sys
+import subprocess
 
-def main:
+def checkDriver():
+    nDriver = subprocess.run(['ls', '/usr/lib'], capture_output=True)
+    nDriverOutput = nDriver.stdout
+    return "nvidia" in str(nDriverOutput)
+
+def main():
+
+# Prints The Opening Screen To Show Script Requirements
     print("   |-------------------------------------------------------|")
     print("   |     NVIDIA - ENABLE FINE-GRAINED POWER MANAGEMENT     |")
     print("   |-------------------------------------------------------|")
     print("   | REQUIREMENTS:                                         |")
-    print("   | 1) Have the NVIDIA Proprietary Driver Installed"      |")
+    print("   | 1) Have the NVIDIA Proprietary Driver Installed       |")
+    print("   | 2) This Script Must Have Sudo Privileges              |")
+    print("   |-------------------------------------------------------|")
+    print("   | REQUIREMENTS MET:                                     |")
+    print("   | 1) ")
 
+
+# Check The Requirements Met
+    driverStatus = checkDriver()
     
+if __name__ == "__main__":
+# This block ensures the main function runs when the script is executed
+# but not when it's imported as a module.
+    try:
+         main()
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+        sys.exit(1)
