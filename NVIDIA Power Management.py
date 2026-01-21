17420e7a79d647376337667ff2a0356938e70c6d
Riley Georgen <rgeorgen10@gmail.com>
2026-01-20 21:24:48 -0700

    Added checkSudo, started CreateRules()

--- NVIDIA Power Management.py
+++ NVIDIA Power Management.py
@@ -1,12 +1,35 @@
 """Python Application To Make The NVIDIA  Driver Ues Fine Power Management"""
 
 import sys
+import os
 import subprocess
 
 def checkDriver():
-    nDriver = subprocess.run(['ls', '/usr/lib'], capture_output=True)  # Gets output from ls /usr/lib
+    nDriver = subprocess.run(['ls', '/usr/lib/systemd/system'], capture_output=True)  # Gets output from ls /usr/lib
     nDriverOutput = nDriver.stdout       # Gets output from nDriver
-    return "nvidia" in str(nDriverOutput)  # Check for nvidia in nDriverOutput and returns result
+    return "nvidia-persistenced" in str(nDriverOutput)  # Check for nvidia in nDriverOutput and returns result
+
+def checkSudo():               
+    if(os.geteuid()==0): # Check if the user id is 0, if it is, then running as sudo
+        return True
+    
+    else:
+        return False
+
+def createRules():
+    try:                                                
+        # try to open 80-nvidia-pm.rules, create if it doesn't exist
+        with open('/lib/udev/rules.d/80-nvidia-pm.rules', 'r') as nvidia-pm:
+            for i in nvidia-pm:
+                if(i=='ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"'):
+                    print("Power Management Rules Are Already In Place")
+                    return
+
+                else:
+                    nvidia-pm.write('# Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind')
+
+        
+    except:
 
 def main():
 
@@ -19,12 +42,38 @@ def main():
     print("   | 2) This Script Must Have Sudo Privileges              |")
     print("   |-------------------------------------------------------|")
     print("   | REQUIREMENTS MET:                                     |")
-    print("   | 1) ")
-
 
 # Check The Requirements Met
     driverStatus = checkDriver()  # Sets driverStatus to true if NVIDIA driver is installed
-    print(driverStatus) 
+    if driverStatus:
+        print("   | 1) NVIDIA DRIVER: INSTALLED                           |")
+
+    else:
+        print("   | 1) NVIDIA DRIVER: NOT INSTALLED                       |")
+        print("   |-------------------------------------------------------| \n")
+        sys.exit(1)
+
+    sudoStatus = checkSudo()  # Set sudoStatus to true if running with root permissions
+    if sudoStatus:
+        print("   | 2) SUDO PRIVILEGES: TRUE                              |")
+    else:
+        print("   | 2) SUDO PRIVILEGES: FALSE                             |")
+        print("   |-------------------------------------------------------| \n")
+        sys.exit(1)
+    
+    print("   |-------------------------------------------------------| \n")
+
+# Continue Once Requirements Have Been Met
+    continueResult = input('Continue With NVIDIA Power Management Configuration (y/n): ')
+
+    if(continueResult=='y' || continueResult=='Y'):
+        print('Creating /lib/udev/rules.d/80-nvidia-pm.rules')
+        createRules()
+
+
+        
+        
+
     
 if __name__ == "__main__":
 # This block ensures the main function runs when the script is executed
